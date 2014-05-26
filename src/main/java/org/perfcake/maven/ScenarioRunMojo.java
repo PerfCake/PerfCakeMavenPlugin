bc62ce4fbdba7f3a430065a97cd21a6e80961091
Vojtech Juranek <vjuranek@redhat.com>
2014-05-26 23:52:50 +0200

    Setup default path to src/test/resources

--- src/main/java/org/perfcake/maven/ScenarioRunMojo.java
+++ src/main/java/org/perfcake/maven/ScenarioRunMojo.java
@@ -16,13 +16,18 @@ package org.perfcake.maven;
  * limitations under the License.
  */
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.perfcake.ScenarioExecution;
 
 /**
@@ -33,6 +38,11 @@ import org.perfcake.ScenarioExecution;
 @Mojo(name = "scenario-run", defaultPhase = LifecyclePhase.INTEGRATION_TEST)
 public class ScenarioRunMojo extends AbstractMojo {
 
+   private final String PERFCAKE_DIR = "perfcake";
+   private final String DEFAULT_SCENARIOS_DIR = PERFCAKE_DIR + File.separator + "scenarios";
+   private final String DEFAULT_MESSAGES_DIR = PERFCAKE_DIR + File.separator + "messages";
+   private final String DEFAULT_PLUGINS_DIR = PERFCAKE_DIR + File.separator +  "plugins";
+
    @Parameter(required = true)
    private String scenario;
    @Parameter(alias = "scenarios-dir")
@@ -41,26 +51,57 @@ public class ScenarioRunMojo extends AbstractMojo {
    private String messagesDir;
    @Parameter(alias = "plugins-dir")
    private String pluginsDir;
-   
+
+   @Component
+   private MavenProject project;
+
    public void execute() throws MojoExecutionException {
+      initDefaults();
+      
       ArrayList<String> args = new ArrayList<String>();
       args.add("-s");
       args.add(scenario);
-      if(scenariosDir != null && !scenariosDir.trim().equals("")) {
-         args.add("-sd");
-         args.add(scenariosDir);
-      }
-      if(messagesDir != null && !messagesDir.trim().equals("")) {
-         args.add("-md");
-         args.add(messagesDir);
-      }
-      if(pluginsDir != null && !pluginsDir.trim().equals("")) {
-         args.add("-pd");
-         args.add(pluginsDir);
-      }
-      
-      System.out.println("Running scenario " + scenario);
+
+      args.add("-sd");
+      args.add(scenariosDir);
+
+      args.add("-md");
+      args.add(messagesDir);
+
+      args.add("-pd");
+      args.add(pluginsDir);
+
+      System.out.println("Running scenario " + scenario); //TODO use log
       ScenarioExecution.main(args.toArray(new String[args.size()]));
-      System.out.println("DONE scenario " + scenario);
+      System.out.println("DONE scenario " + scenario); //TODO use log
    }
+
+   private void initDefaults() {
+      String resPath = getDefaultResourcePath();
+
+      if (scenariosDir == null || scenariosDir.trim().isEmpty()) {
+         File testDir = new File(resPath + File.separator + DEFAULT_SCENARIOS_DIR);
+         scenariosDir = testDir.isDirectory() ? testDir.getAbsolutePath() : resPath;
+      }
+
+      if (messagesDir == null || messagesDir.trim().isEmpty()) {
+         File testDir = new File(resPath + File.separator + DEFAULT_MESSAGES_DIR);
+         messagesDir = testDir.isDirectory() ? testDir.getAbsolutePath() : resPath;
+      }
+
+      if (pluginsDir == null || pluginsDir.trim().isEmpty()) {
+         File testDir = new File(resPath + File.separator+ DEFAULT_PLUGINS_DIR);
+         pluginsDir = testDir.isDirectory() ? testDir.getAbsolutePath() : resPath;
+      }
+   }
+
+   private String getDefaultResourcePath() {
+      String defResPath = null;
+      List<Resource> defResList = project.getBuild().getTestResources();
+      if (!defResList.isEmpty() && defResList.get(0) != null) {
+         defResPath = defResList.get(0).getDirectory();
+      }
+      return defResPath;
+   }
+
 }
